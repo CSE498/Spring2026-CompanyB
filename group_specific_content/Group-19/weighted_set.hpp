33d031b95d4cce545f75707f0ba471d1d579bd3e
Luke Bridges <lukebridges123@gmail.com>
2026-02-02 21:41:44 -0500

    Start using an unorderd_map as part of weighted_set; first try at "insert"

--- group_specific_content/Group-19/weighted_set.hpp
+++ group_specific_content/Group-19/weighted_set.hpp
@@ -1,27 +1,29 @@
 #pragma once
 #include <cstddef>
+#include <unordered_map>
 #include <utility>
 
 namespace cse498 {
 /*
   Forgot to mention this in earlier commit
-  But the boilerplate setup (constructors, clear() and copy_tree(), assign and move operators,
-  basic getters [namely size(), empty(), total_weight()], and basic member variables) 
-  was written by Claude Code with (so far moderate) modification from me.
+  But the boilerplate setup (constructors, clear() and copy_tree(), assign and
+  move operators, basic getters [namely size(), empty(), total_weight()], and
+  basic member variables) was written by Claude Code with (so far moderate)
+  modification from me.
 */
 template <typename T>
 class WeightedSet {
  private:
   struct Node {
-    T value;
+    T* value_ptr;
     double weight;          // this element's weight (nonnegative)
     double subtree_weight;  // total weight of subtree rooted at this node
-    Node* left;
-    Node* right;
+    Node* left = nullptr;
+    Node* right = nullptr;
     Node* parent;
 
     Node(const T& val, double w, Node* par = nullptr)
-        : value(val),
+        : value_ptr(&val),
           weight(w),
           subtree_weight(w),
           left(nullptr),
@@ -31,6 +33,8 @@ class WeightedSet {
 
   Node* root_;
   size_t size_;
+
+  std::unordered_map<T, Node*> element_to_node_;
 
   void clear(Node* node) {
     if (!node) return;
@@ -72,6 +76,40 @@ class WeightedSet {
       other.size_ = 0;
     }
     return *this;
+  }
+
+  bool insert(T element, double weight) {
+    if (this->root_ == nullptr) {
+      this->root_ = new Node(element, weight);
+      return true;
+    }
+
+    if (this->element_to_node_.find(element) != element_to_node_.end()) {
+      this->element_to_node_.at(element)->weight = weight;
+      // TODO: rebalance and update parents
+    }
+    T* element_ptr = new T{element};
+    Node* node_ptr = new Node(element_ptr, weight);
+    this->element_to_node_.insert(*element_ptr, node_ptr);
+    Node* current_node = this->root_;
+    while (current_node != nullptr) {
+      current_node->subtree_weight += weight;
+      if (current_node->left == nullptr) {
+        current_node->left = node_ptr;
+        node_ptr->parent = current_node->left;
+        break;
+      } else if (current_node->right == nullptr) {
+        current_node->right = node_ptr;
+        node_ptr->parent = current_node->right;
+        break;
+      } else if (current_node->left->subtree_weight <
+                 current_node->right->subtree_weight) {
+        current_node = current_node->left;
+      } else {
+        current_node = current_node->right;
+      }
+    }
+    return true;
   }
 
   ~WeightedSet() { clear(root_); }
