56eca29f8d114a6d1711e100e72ab19c3d9af11d
kiewskim <kiewskim@msu.edu>
2026-02-05 00:26:07 -0500

    Update ImageGrid

---
+++ source/Interfaces/gui/ImageGrid.hpp
@@ -0,0 +1,4 @@
+/**
+ * This file is part of the Fall 2026, CSE 498, section 2, course project.
+ * @brief A class that replaces certain cell types with corresponding images (ie. replaces '#' with a road tile)
+ **/
