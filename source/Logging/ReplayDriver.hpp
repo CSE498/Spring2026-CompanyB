a4d3a705ee1408c05535684a8a21ede2d629561c
Apar <apar.mohabansi@gmail.com>
2026-02-10 01:58:35 -0500

    Initial version of ReplayDriver, just barebones skeleton

---
+++ source/Logging/ReplayDriver.hpp
@@ -0,0 +1,25 @@
+/**
+ * This file is for the reading and replaying of a save file.
+ * @brief API class for data replay.
+ * @note Status: INITIAL DESIGN
+ **/
+
+#pragma once
+
+#include "nlohmann/json.hpp"
+
+namespace cse498 {
+    class ReplayDriver {
+        private:
+            // need a pointer to the world object to instuct addtions and deletions of agents from world
+        public:
+            /// @brief Basic constructor for the ReplayDriver.
+            ReplayDriver() = default;
+            ~ReplayDriver() = default;
+
+            /// @brief Method to replay logged events from a JSON file.
+            /// @param filePath Path to the JSON file containing logged events.
+            /// @return Success status of the replay operation.
+            bool ReplayFromFile(const std::string& filePath);
+    };
+}
