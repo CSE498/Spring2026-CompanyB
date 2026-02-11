d938eee11258cbb2a78032db4eb6b81f823af929
Nicholas L McComb <nick.mccomb062@gmail.com>
2026-02-10 23:06:14 -0500

    Added one test case to AnnotationSetTest.cpp

--- group_specific_content/Group-12/AnnotationSetTest.cpp
+++ group_specific_content/Group-12/AnnotationSetTest.cpp
@@ -6,17 +6,26 @@
  */
 
 #define CATCH_CONFIG_MAIN
-#include "../../../third-party/Catch/single_include/catch2/catch.hpp"
+#include "../../third-party/Catch/single_include/catch2/catch.hpp"
 #include <string>
 #include <climits>
 
 #include "AnnotationSet.hpp"
 
+// g++ -std=c++23 -Wall -Wextra -I../../../third-party/Catch/single_include AnnotationSet.cpp AnnotationSetTest.cpp -o AnnotationSetTest.exe
 
 //constructor 
 
-TEST_CASE("Annotation set constructor", "[AnnotationSet]"){
+TEST_CASE("AnnotationSet constructor", "[Constructor]"){
     AnnotationSet s;
     CHECK(s.size() == 0);
     CHECK(s.empty() == true);
+}
+
+//insert
+TEST_CASE("AnnotationSet AddTag", "[AddTag]"){
+    AnnotationSet s;
+    s.addTag("tag1");
+    CHECK(s.size() == 1);
+    CHECK(s.hasTag("tag1") == true);
 }
