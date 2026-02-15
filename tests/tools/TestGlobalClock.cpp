997e87b0cd2fe709a8b61855c781167a321c9c61
Abdulrahman Alanazi <alanazi7@msu.edu>
2026-02-15 16:36:59 -0500

    Added test cases for GlobalClock class to test its functionality.

---
+++ tests/tools/TestGlobalClock.cpp
@@ -0,0 +1,25 @@
+#define CATCH_CONFIG_MAIN
+#include "../../third-party/Catch/single_include/catch2/catch.hpp"
+#include "../../source/tools/GlobalClock.hpp"
+
+
+
+TEST_CASE("Test initial values of Global Clock", "[GlobalClock]") {
+    cse498::GlobalClock::Reset();
+    REQUIRE(cse498::GlobalClock::GetTime() == 0);
+
+}
+TEST_CASE("Test Tick method", "[GlobalClock]") {
+    cse498::GlobalClock::Reset();
+    cse498::GlobalClock::Tick(10);
+    REQUIRE(cse498::GlobalClock::GetTime() == 10);
+    cse498::GlobalClock::Tick(100);
+    REQUIRE(cse498::GlobalClock::GetTime() == 110);
+    cse498::GlobalClock::Reset();
+    REQUIRE(cse498::GlobalClock::GetTime() == 0);
+    cse498::GlobalClock::Tick(0);
+    REQUIRE(cse498::GlobalClock::GetTime() == 0);
+
+    //Beyond this point, it appears that if I keep continuing using tick method without reset, I will not be able to get the values right.
+
+}
