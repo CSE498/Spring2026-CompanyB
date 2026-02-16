c0004b631f27d1c095dbeb3368dddf45069ba928
Scott2444 <scotty.haakenson@gmail.com>
2026-02-15 23:04:36 -0500

    Action log created, unit tests created

--- source/tools/ActionLog.cpp
+++ source/tools/ActionLog.cpp
@@ -6,10 +6,53 @@
 
 namespace cse498 {
 
+    static const std::array<std::string, 4> log_levels = {
+        "DEBUG",
+        "INFO",
+        "WARNING",
+        "ERROR"
+    };
+
+    bool CheckRequiredFields(const nlohmann::json& data) {
+        // // Timestamp must be a non-negative integer
+        // if (!data.contains("timestamp") || 
+        //     !data["timestamp"].is_number_integer() ||
+        //     data["timestamp"].get<int64_t>() <= 0
+        // ) {
+        //     return false;
+        // }
+        
+        // Event type must be a non-empty string
+        if (!data.contains("event_type") || 
+            !data["event_type"].is_string() ||
+            data["event_type"].get<std::string>().empty()
+        ) {
+            return false;
+        }
+
+        // Log level must be one of the predefined log levels
+        if (!data.contains("log_level") || 
+            !data["log_level"].is_string() ||
+            (std::find(log_levels.begin(), log_levels.end(), data["log_level"].get<std::string>()) == log_levels.end())
+        ) {
+            return false;
+        }
+
+        // Details must be a JSON object
+        if (!data.contains("details") || 
+            !data["details"].is_object()
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
     LogEventStatus ActionLog::LogEvent(const nlohmann::json& data) {
-        // No enforcement or validation on data for now.
+        if (!CheckRequiredFields(data)) {
+            return LogEventStatus::FAILURE;
+        }
         dataLog.AddEntry(data);
         return LogEventStatus::SUCCESS;
     }
-
 }
