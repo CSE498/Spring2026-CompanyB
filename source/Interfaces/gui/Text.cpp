08952ee59e8b9231f333b8b0c2c3421f602808c0
divyadella <70812705+divyadella@users.noreply.github.com>
2026-02-16 21:55:45 -0500

    Added the class implementations and test suite for Text

--- source/Interfaces/gui/Text.cpp
+++ source/Interfaces/gui/Text.cpp
@@ -1,40 +1,116 @@
 #include "Text.hpp"
 
+#include "../../../third-party/qt/include/QtCore/QString"
+#include "../../../third-party/qt/include/QtGui/QFont"
+#include "../../../third-party/qt/include/QtGui/QColor"
+
+// for sample display
+#include "../../../third-party/qt/include/QtWidgets/QApplication"
+#include "../../../third-party/qt/include/QtWidgets/QPushButton"
+#include "../../../third-party/qt/include/QtWidgets/QGraphicsScene"
+#include "../../../third-party/qt/include/QtWidgets/QGraphicsTextItem"
+#include "../../../third-party/qt/include/QtWidgets/QGraphicsView"
+
 namespace cse498 {
 
-// constructor 
-Text::Text(string content, string font, string color, int size, bool bold, bool underline, bool italics, bool uppercase){
+// ----------------------------------constructor---------------------------------------- //
+Text::Text(QString content, QString font, QString color, float size, bool bold, bool underline, bool italics, bool uppercase){
     mContent = content;
-
-    mFont = font;
-    mColor = color;
+    mFont = QFont(font);
+    mColor = QColor(color);
     mSize = size;
     mBold = bold;
     mUnderline = underline;
     mItalics = italics;
     mUppercase = uppercase;
+
+    mFont.setPointSizeF(size);
+    mFont.setBold(bold);
+    mFont.setUnderline(underline);
+    mFont.setItalic(italics);
+
+    if (uppercase == true) 
+        mFont.setCapitalization(QFont::AllUppercase);
+    else 
+        mFont.setCapitalization(QFont::MixedCase);
 }
 
-// getters
-string Text::GetContent(){ return mContent; } 
+// ------------------------------------getters------------------------------------------ //
+QString Text::GetContent(){ return mContent; } 
 
-string Text::GetFont(){ return mFont; } 
-string Text::GetColor(){ return mColor; } 
-int Text::GetSize(){ return mSize; } 
+QFont Text::GetFont(){ return mFont; } 
+QColor Text::GetColor(){ return mColor; } 
+float Text::GetSize(){ return mSize; } 
 bool Text::GetBold(){ return mBold; } 
 bool Text::GetUnderline(){ return mUnderline; } 
 bool Text::GetItalics(){ return mItalics; } 
 bool Text::GetUppercase(){ return mUppercase; } 
 
-// setters
-void Text::SetContent(string content){ mContent = content; } 
+// ------------------------------------setters------------------------------------------ //
+void Text::SetContent(QString content){ mContent = content; } 
 
-void Text::SetFont(string font){ mFont = font; } 
-void Text::SetColor(string color){ mColor = color; } 
-void Text::SetSize(int size){ mSize = size; } 
-void Text::SetBold(bool bold){ mBold = bold; } 
-void Text::SetUnderline(bool underline){ mUnderline = underline; } 
-void Text::SetItalics(bool italics){ mItalics = italics; } 
-void Text::SetUppercase(bool uppercase){ mUppercase = uppercase; } 
+void Text::SetFont(QString font){ mFont = QFont(font); } 
+void Text::SetColor(QString color){ mColor = QColor(color); } 
 
+void Text::SetSize(float size){ 
+    mSize = size;
+    mFont.setPointSizeF(size);
+ }
+
+void Text::SetBold(bool bold){
+    mBold = bold;
+    mFont.setBold(bold);
+} 
+void Text::SetUnderline(bool underline){ 
+    mUnderline = underline; 
+    mFont.setUnderline(underline);
+} 
+void Text::SetItalics(bool italics){ 
+    mItalics = italics; 
+    mFont.setItalic(italics);
+} 
+
+void Text::SetUppercase(bool uppercase){ 
+    mUppercase = uppercase; 
+
+    if (uppercase == true) 
+        mFont.setCapitalization(QFont::AllUppercase);
+    else 
+        mFont.setCapitalization(QFont::MixedCase);
+} 
+
+// ------------------------------------display------------------------------------------ //
+void Text::displayText(QGraphicsScene& scene){
+    QGraphicsTextItem* textItem = scene.addText(mContent);
+    textItem->setFont(mFont);
+    textItem->setDefaultTextColor(mColor);
 }
+}
+
+// -------------------------------------main-------------------------------------------- //
+// this main function won't be in the final class implementation, it exists to test out the class
+/*
+int main(int argc, char **argv)
+{
+ QApplication app (argc, argv);
+
+QGraphicsScene scene;
+QGraphicsView view(&scene);
+
+cse498::Text helloText("hello", "Arial", "blue", 30, true, false, true, true);
+helloText.displayText(scene);
+
+cse498::Text byeText("bye", "Garamond", "#751401", 50, false, true, false, false);
+byeText.displayText(scene);
+*/
+
+/* the texts do overlap when displayed, but we are still working on the GUI so it is currently
+   (or maybe completely) out of my jurisdiction to modify displayText to take in positions */
+
+/*
+view.show();
+
+return app.exec();
+}
+*/
+// ------------------------------------------------------------------------------------- //
--- source/Interfaces/gui/Text.hpp
+++ source/Interfaces/gui/Text.hpp
@@ -1,8 +1,9 @@
 #pragma once 
 
-#include <iostream>
-#include <string>
-using std::string;
+#include "../../../third-party/qt/include/QtCore/QString"
+#include "../../../third-party/qt/include/QtGui/QFont"
+#include "../../../third-party/qt/include/QtGui/QColor"
+#include "../../../third-party/qt/include/QtWidgets/QGraphicsScene"
 
 namespace cse498 {
 
@@ -11,11 +12,10 @@ class Text {
 private:
 
     // member variables 
-    string mContent;
-
-    string mFont;
-    string mColor;
-    int mSize;
+    QString mContent;
+    QFont mFont;
+    QColor mColor;
+    float mSize;
     bool mBold;
     bool mUnderline;
     bool mItalics;
@@ -23,30 +23,31 @@ private:
 
 public:
 
-    // constructor
-    Text(string content, string font, string color, int size, bool bold, bool underline, bool italics, bool uppercase);
+    // ----------------------------------constructor---------------------------------------- //
+    Text(QString content, QString font, QString color, float size, bool bold, bool underline, bool italics, bool uppercase);
 
-    // getters
-    string GetContent();
-
-    string GetFont();
-    string GetColor();
-    int GetSize();
+    // ------------------------------------getters------------------------------------------ //
+    QString GetContent();
+    QFont GetFont();
+    QColor GetColor();
+    float GetSize();
     bool GetBold();
     bool GetUnderline();
     bool GetItalics();
     bool GetUppercase();
 
-    // setters 
-    void SetContent(string content);
-
-    void SetFont(string font);
-    void SetColor(string color);
-    void SetSize(int size);
+    // ------------------------------------setters------------------------------------------ //
+    void SetContent(QString content);
+    void SetFont(QString font);
+    void SetColor(QString color);
+    void SetSize(float size);
     void SetBold(bool bold);
     void SetUnderline(bool underline);
     void SetItalics(bool italics);
     void SetUppercase(bool uppercase);
+
+    // ------------------------------------display------------------------------------------ //
+    void displayText(QGraphicsScene& scene);
 
 };
 
