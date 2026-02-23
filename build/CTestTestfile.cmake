b8ca995c9877b3f39a15ba02de218ccc9432fc02
annikavalaboju <valaboj1@msu.edu>
2026-02-22 19:19:34 -0500

    Remove build directory and ignore

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/annikavalaboju/498 CAPSTONE/Spring2026-CompanyB/third-party/Catch
-# Build directory: /Users/annikavalaboju/498 CAPSTONE/Spring2026-CompanyB/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("projects")
