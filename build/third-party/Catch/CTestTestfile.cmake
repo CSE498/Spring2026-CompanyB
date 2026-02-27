21462e745f4f238b5264864ac86e3dd7f8cc8053
Benjamin Harrison <harr1890@msu.edu>
2026-02-27 01:37:10 -0500

    deleted unwanted files. added new dir teampdftxtfiles in group-specific for team files

--- build/third-party/Catch/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/rancho/Desktop/capstone/Spring2026-CompanyB/third-party/Catch
-# Build directory: /Users/rancho/Desktop/capstone/Spring2026-CompanyB/build/third-party/Catch
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
