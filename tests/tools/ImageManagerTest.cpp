f38ce4bcd86399df53a52669f44f454304aaecaa
Andreea Danila <danilaan@msu.edu>
2026-02-27 13:26:47 -0500

    imagemanager

--- tests/tools/ImageManagerTest.cpp
+++ tests/tools/ImageManagerTest.cpp
@@ -2,75 +2,40 @@
  * @file ImageManagerTest.cpp
  * @author Andreea Danila
  */
+//To run 'g++ ImageManagerTest.cpp ..\..\source\Interfaces\gui\ImageManager.cpp -o ImageManagerTest'
+//To run test write  './ImageManagerTest'
+
+
+#define CATCH_CONFIG_MAIN
+#include "../../third-party/Catch/single_include/catch2/catch.hpp"
 
 #include "../../source/Interfaces/gui/ImageManager.hpp"
 
-#include "../../source/Interfaces/gui/ImageManager.cpp"
-
-#include <cassert>
-
-int main() {
-
-    // Test constructor
+TEST_CASE("Test ImageManager Constructor", "[ImageManager]")
+{
     cse498::ImageManager manager1;
-    assert(!manager1.HasImage("anyImage"));
-    assert(!manager1.HasImage(""));
+    REQUIRE(!manager1.HasImage("anyImage"));
+    REQUIRE(!manager1.HasImage(""));
+}
 
-    // Test first image
+
+TEST_CASE("Test ImageManager Remove", "[ImageManager]")
+{
+    cse498::ImageManager manager1;
     manager1.Load("image1", "../../images/test1.png");
-    assert(manager1.HasImage("image1"));
-    assert(manager1.GetImage("image1") == "../../images/test1.png");
-
-    // Test second image
     manager1.Load("image2", "../../images/test2.png");
-    assert(manager1.HasImage("image1"));
-    assert(manager1.HasImage("image2"));
-    assert(manager1.GetImage("image2") == "../../images/test2.png");
 
-    // Test AddToImages method
-    manager1.AddToImages("image3", "/path/to/image3.png");
-    assert(manager1.HasImage("image3"));
-    assert(manager1.GetImage("image3") == "/path/to/image3.png");
-    assert(manager1.HasImage("image1")); // Previous images still there
-    assert(manager1.HasImage("image2"));
-
-    // Test overwriting an existing image
-    manager1.Load("image1", "/path/to/new_image1.png");
-    assert(manager1.HasImage("image1"));
-    assert(manager1.GetImage("image1") == "/path/to/new_image1.png");
-    assert(manager1.GetImage("image1") != "/path/to/image1.png");
-
-    // Test Remove
     manager1.Remove("image2");
-    assert(!manager1.HasImage("image2"));
-    assert(manager1.HasImage("image1"));
-    assert(manager1.HasImage("image3"));
+    REQUIRE(!manager1.HasImage("image2"));
+    REQUIRE(manager1.HasImage("image1"));
+}
 
-    // Test Clear
+TEST_CASE("Test ImageManager Clear", "[ImageManager]")
+{
+    cse498::ImageManager manager1;
+    manager1.Load("image1", "../../images/test1.png");
+
     manager1.Clear();
-    assert(!manager1.HasImage("image1"));
-    assert(!manager1.HasImage("image3"));
+    REQUIRE(!manager1.HasImage("image1"));
 
-    // Test that we can add after clear
-    manager1.AddToImages("newImage", "/path/new.png");
-    assert(manager1.HasImage("newImage"));
-
-    // Test empty ImageID
-    cse498::ImageManager manager2;
-    manager2.Load("", "/path/to/image.png");
-    assert(!manager2.HasImage(""));
-
-    // Test empty path
-    manager2.Load("testImage", "");
-    assert(!manager2.HasImage("testImage"));
-
-    // Test AddToImages with empty ImageID
-    manager2.AddToImages("", "/path/to/image.png");
-    assert(!manager2.HasImage(""));
-
-    // Test AddToImages with empty path
-    manager2.AddToImages("testImage", "");
-    assert(!manager2.HasImage("testImage"));
-
-    return 0;
 }
