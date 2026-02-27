1afb47243a9e8c7ae5b684e4b26fcca500f9c812
Ebenezer Appiah <103301225+Appiaheb@users.noreply.github.com>
2026-02-26 23:10:01 -0500

    Update Box_test.cpp
    updated some header file and included these:
    
    #include "../source/tools/Box.hpp"    and
    
    #include "../third-party/Catch/single_include/catch2/catch.hpp"

--- group_specific_content/Group-13/Box/Box_test.cpp
+++ group_specific_content/Group-13/Box/Box_test.cpp
@@ -5,10 +5,11 @@
  * @author Ebenezer Appiah (Group 13 - Math-Heavy Simulation World)
  */
 
-#include "Box.hpp"
-#include "PointClass.hpp"
+#include "../source/tools/Box.hpp"
+#include "Point.hpp"
 #include "Circle.hpp"
-#include <catch2/catch_test_macros.hpp>
+#include "../third-party/Catch/single_include/catch2/catch.hpp"
+#include <iostream>
 #include <cmath>
 #include <sstream>
 
