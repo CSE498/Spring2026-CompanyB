aef2ca2040e99d846b4cf6edd28cebed3a9185ff
Luke Bridges <lukebridges123@gmail.com>
2026-02-27 19:53:35 -0500

    Change WeightedSet to use new Random function names + get rid of binary

--- source/tools/WeightedSet.hpp
+++ source/tools/WeightedSet.hpp
@@ -432,7 +432,7 @@ class WeightedSet {
     if (!root_) {
       return std::nullopt;
     }
-    return GetElementAt(rng_.GetDouble(0, total_weight()));
+    return GetElementAt(rng_.nextDouble(0, total_weight()));
   }
 
   /**
