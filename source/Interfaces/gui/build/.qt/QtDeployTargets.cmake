5f50cf27b1979b60360802668ff42ac3601096e1
Andreea Danila <danilaan@msu.edu>
2026-03-13 03:51:21 -0400

    Removed nested build folder from tracking

--- source/Interfaces/gui/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ImageManagerApp_FILE C:/Users/munni/Spring2026-CompanyB/source/Interfaces/gui/build/ImageManagerApp.exe)
-set(__QT_DEPLOY_TARGET_ImageManagerApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ImageManagerApp_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
