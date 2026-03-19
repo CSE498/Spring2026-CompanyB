6edd8b04a6bf1f735b24c33b25fec3d8e495a23c
Kira Chan <tokirachan@gmail.com>
2026-03-18 21:06:53 -0400

    Delete source/tools/clean.sh

--- source/tools/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e " ==> Deleting all .out files <== \n"
-
-# for file in *.out; do
-find . -type f \( -name "*.out" \) | while read -r file; do
-    echo "Deleting $file"
-    rm "$file"
-done
