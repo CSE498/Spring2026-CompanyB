68e00c0727f1abfc48301b283909df4e11224f74
Kira Chan <tokirachan@gmail.com>
2026-03-18 21:06:42 -0400

    Delete source/tools/compileall.sh

--- source/tools/compileall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-# for file in *.cpp *.hpp; do
-find . -type f \( -name "*.cpp" -o -name "*.hpp" \) | while read -r file; do
-    dir=$(dirname "$file")
-    base=$(basename "$file")
-    c++ $file -std=c++23 -Wall -Wextra -Wcast-align -Wnon-virtual-dtor -Woverloaded-virtual -pedantic -c -o "$dir/_$base".out
-done
