95230d5951b29b21c8ae75e4ce2fbdaf4c157587
Luke Bridges <lukebridges123@gmail.com>
2026-03-22 10:44:08 -0400

    Simple demo main

---
+++ source/traffic_main.cpp
@@ -0,0 +1,21 @@
+/**
+ * This file is part of the Fall 2026, CSE 498, section 2, course project.
+ * @brief A simplistic main file to demonstrate a system.
+ * @note Status: PROPOSAL
+ **/
+
+// Include the modules that we will be using.
+#include "Agents/DrivingAgent.hpp"
+#include "Interfaces/TrashInterface.hpp"
+#include "Worlds/TrafficWorld.hpp"
+
+using namespace cse498;
+
+int main() {
+  TrafficWorld world;
+  world.AddAgent<DrivingAgent>("Car 1").SetLocation(WorldPosition{5, 5});
+  world.AddAgent<TrashInterface>("Interface")
+      .SetSymbol('@');
+
+  world.Run();
+}
