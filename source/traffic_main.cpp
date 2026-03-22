5bbae03969008a0cfd889867d7b2ed5aeb78c808
Luke Bridges <lukebridges123@gmail.com>
2026-03-22 17:04:48 -0400

    Add another car

--- source/traffic_main.cpp
+++ source/traffic_main.cpp
@@ -14,6 +14,7 @@ using namespace cse498;
 int main() {
   TrafficWorld world;
   world.AddAgent<DrivingAgent>("Car 1").SetLocation(WorldPosition{5, 6});
+  world.AddAgent<DrivingAgent>("Car 2").SetDirection(Direction::South).SetLocation(WorldPosition{8, 4});
   world.AddAgent<TrashInterface>("Interface")
       .SetSymbol('@');
 
