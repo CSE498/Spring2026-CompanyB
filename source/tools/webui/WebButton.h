6450c67b9e9908ab3130b186f1b975ccee745391
Fatima402 <fatimasaad21@outlook.com>
2026-03-23 16:05:51 -0400

    Add WebButton implementation and tests

--- source/tools/webui/WebButton.h
+++ source/tools/webui/WebButton.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <emscripten/val.h>
 #include <functional>
 #include <string>
 
@@ -10,38 +11,28 @@
  * This class stores button state (label, visibility, enabled) and a click
  * callback
  */
+
+namespace cse498 {
+
 class WebButton {
  public:
-  /// Construct a WebButton with a label
-  explicit WebButton(const std::string& label, const std::string& id = "");
+  explicit WebButton(const std::string& label, const std::string& id);
+  ~WebButton();
 
-  /// Get/Set the label
   const std::string& GetLabel() const;
   void SetLabel(const std::string& label);
 
-  /// Get/Set an optional DOM id
   const std::string& GetId() const;
-  void SetId(const std::string& id);
 
-  /// Show/Hide  button is visible.
   void Show();
   void Hide();
   bool IsVisible() const;
 
-  /// Enable/Disable controls whether the button can be clicked.
   void Enable();
   void Disable();
   bool IsEnabled() const;
 
-  /// Register a callback to run when the button is clicked.
   void SetOnClick(std::function<void()> callback);
-
-  /**
-   * @brief Simulate a click for testing.
-   *
-   * Runs the callback only if visible and enabled and a callback is set.
-   * (In the browser build, this will be triggered by a DOM event.)
-   */
   void Click();
 
  private:
@@ -50,4 +41,7 @@ class WebButton {
   bool visible_{true};
   bool enabled_{true};
   std::function<void()> on_click_{};
+  emscripten::val button_element_{emscripten::val::undefined()};
 };
+
+}
