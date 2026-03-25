1f2646ff97393512655565afcae5539d3e5402b9
udbhav-s <udbhavs03@gmail.com>
2026-03-25 15:53:55 -0400

    update mock dom to jsdom for WebButton tests

--- tests/wasm/tools/webui/test_WebButton.cpp
+++ tests/wasm/tools/webui/test_WebButton.cpp
@@ -7,110 +7,76 @@
 
 using namespace cse498;
 
-struct SetupMockDOM {
-  SetupMockDOM() {
+struct SetupMockDOMWebButton {
+  SetupMockDOMWebButton() {
     // clang-format off
     EM_ASM({
+      /// Creates a document if it doesn't exist
       if (typeof document === 'undefined') {
-        globalThis.document = {};
+        const { JSDOM } = jsdom;
+        const dom = new JSDOM("<!DOCTYPE html> <html><head></head><body></body></html>");
+        globalThis.window = dom.window;
+        globalThis.document = dom.window.document;
       }
-
-      var elements = {};
-
-      document.getElementById = function(id) { return elements[id] || null; };
-
-      document.createElement = function(tag) {
-        var elem = {};
-        elem._id = "";
-        elem.innerText = "";
-        elem.disabled = false;
-        elem.style = {};
-        elem.style.display = "";
-
-        Object.defineProperty(elem, 'id', {
-          get: function() { return elem._id; },
-          set: function(v) {
-            elem._id = v;
-            elements[v] = elem;
-          }
-        });
-
-        elem.remove = function() {
-          if (elem._id && elements[elem._id]) {
-            delete elements[elem._id];
-          }
-        };
-
-        return elem;
-      };
-
-      if (!document.body) {
-        document.body = {};
-      }
-
-      document.body.appendChild = function(elem) { return elem; };
-      document.body.removeChild = function(elem) {};
-
     });
     // clang-format on
-    }
+  }
 
-    ~SetupMockDOM() {
-      // clang-format off
-    EM_ASM({
-      delete globalThis.document;
-      delete globalThis.window;
-      delete globalThis.Image;
-    });
-      // clang-format on
-    }
-    }
-    ;
+  ~SetupMockDOMWebButton() {
+    // clang-format off
+      EM_ASM({
+        delete globalThis.document;
+        delete globalThis.window;
+      });
+    // clang-format on
+  }
+};
 
-    TEST_CASE("WebButton basics") {
-      SetupMockDOM mock;
-      cse498::WebButton b("Start", "startBtn");
 
-      SECTION("Label set/get works") {
-        REQUIRE(b.GetLabel() == "Start");
-        b.SetLabel("Pause");
-        REQUIRE(b.GetLabel() == "Pause");
-      }
+TEST_CASE("WebButton basics") {
+  SetupMockDOMWebButton mock;
+  cse498::WebButton b("Start", "startBtn");
 
-      SECTION("Visibility works") {
-        REQUIRE(b.IsVisible());
-        b.Hide();
-        REQUIRE_FALSE(b.IsVisible());
-        b.Show();
-        REQUIRE(b.IsVisible());
-      }
+  SECTION("Label set/get works") {
+    REQUIRE(b.GetLabel() == "Start");
+    b.SetLabel("Pause");
+    REQUIRE(b.GetLabel() == "Pause");
+  }
 
-      SECTION("Enable/Disable works") {
-        REQUIRE(b.IsEnabled());
-        b.Disable();
-        REQUIRE_FALSE(b.IsEnabled());
-        b.Enable();
-        REQUIRE(b.IsEnabled());
-      }
+  SECTION("Visibility works") {
+    REQUIRE(b.IsVisible());
+    b.Hide();
+    REQUIRE_FALSE(b.IsVisible());
+    b.Show();
+    REQUIRE(b.IsVisible());
+  }
 
-      SECTION("Click runs callback only when enabled and visible") {
-        int count = 0;
-        b.SetOnClick([&count]() { ++count; });
+  SECTION("Enable/Disable works") {
+    REQUIRE(b.IsEnabled());
+    b.Disable();
+    REQUIRE_FALSE(b.IsEnabled());
+    b.Enable();
+    REQUIRE(b.IsEnabled());
+  }
 
-        b.Click();
-        REQUIRE(count == 1);
+  SECTION("Click runs callback only when enabled and visible") {
+    int count = 0;
+    b.SetOnClick([&count]() { ++count; });
 
-        b.Disable();
-        b.Click();
-        REQUIRE(count == 1);
+    b.Click();
+    REQUIRE(count == 1);
 
-        b.Enable();
-        b.Hide();
-        b.Click();
-        REQUIRE(count == 1);
+    b.Disable();
+    b.Click();
+    REQUIRE(count == 1);
 
-        b.Show();
-        b.Click();
-        REQUIRE(count == 2);
-      }
-    }
+    b.Enable();
+    b.Hide();
+    b.Click();
+    REQUIRE(count == 1);
+
+    b.Show();
+    b.Click();
+    REQUIRE(count == 2);
+  }
+}
