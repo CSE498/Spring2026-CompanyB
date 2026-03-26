0fc2ee0071e7fafa5ce2960b2fa038823f08e758
Luke Bridges <lukebridges123@gmail.com>
2026-03-25 22:14:06 -0400

    More comments in demo

--- demo/Group19_demo.cpp
+++ demo/Group19_demo.cpp
@@ -1,21 +1,23 @@
 /**
  * This file is part of the Fall 2026, CSE 498, section 2, course project.
- * @brief A simplistic main file to demonstrate a system.
+ * @brief A demo of group 19's TrafficWorld class.
  * @note Status: PROPOSAL
  **/
 
-// Include the modules that we will be using.
-#include "../source/Agents/DrivingAgent.hpp"
 #include "../source/Interfaces/AutoInterface.hpp"
 #include "../source/Worlds/TrafficWorld.hpp"
 
 using namespace cse498;
 
 int main() {
+  // At this point, there isn't much you can do with TrafficWorld besides make it. 
+  // You can add agents in the same way as any world, but it spawns agents on its own, so you don't need to.
+  // Also note that TrafficWorld is only fully compatible with DrivingAgent right now. This is not something
+  // we want to have long-term, of course, but until the agent-world API is fully worked out, we figure we'll
+  // just use a single simple agent to show things off.
   TrafficWorld world;
-  // world.AddAgent<DrivingAgent>("Car 1").SetLocation(WorldPosition{5, 6});
-  // world.AddAgent<DrivingAgent>("Car
-  // 2").SetDirection(Direction::South).SetLocation(WorldPosition{8, 4});
+  // AutoInterface is a little class we made to replace TrashInterface for the specific purpose
+  // of 
   world.AddAgent<AutoInterface>("Viewer");
 
   world.Run();
