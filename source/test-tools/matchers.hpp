c863c5c82e8779858991e87e0b00c522654b8f4d
Cole Scheller <schelcc@pobox.com>
2026-04-01 11:27:33 -0400

    Additional matcher to verify expected has value

--- source/test-tools/matchers.hpp
+++ source/test-tools/matchers.hpp
@@ -4,6 +4,7 @@
 #include <boost/core/demangle.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_templated.hpp>
+#include <expected>
 #include <optional>
 #include <sstream>
 #include <string>
@@ -18,6 +19,15 @@ template <typename T> struct OptNotNull : Catch::Matchers::MatcherGenericBase {
   std::string describe() const override { return "Has value (is not empty)"; }
 };
 
+struct ExpNotErr : Catch::Matchers::MatcherGenericBase {
+  template <typename T, typename E>
+  bool match(std::expected<T, E> const &other) const {
+    return other.has_value();
+  }
+
+  std::string describe() const override { return "Has value (is not err)"; }
+};
+
 template <typename State>
 struct VariantState : Catch::Matchers::MatcherGenericBase {
   template <typename... Types>
@@ -27,7 +37,12 @@ struct VariantState : Catch::Matchers::MatcherGenericBase {
 
   // Gotta be a better way, like piping matcher results or something
   template <typename... Types>
-  bool match(std::optional<std::variant<Types...>> other) const {
+  bool match(std::optional<std::variant<Types...>> const &other) const {
+    return std::holds_alternative<State>(other.value());
+  }
+
+  template <typename... Types, typename E>
+  bool match(std::expected<std::variant<Types...>, E> const &other) const {
     return std::holds_alternative<State>(other.value());
   }
 
