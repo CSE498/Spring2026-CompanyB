f0da8be980ad27c6cb1ff082a0e3b349658b3c3c
Cole Scheller <schelcc@pobox.com>
2026-03-31 22:37:40 -0400

    More extensive test for a conditional add, as well as helper matchers for Catch2

---
+++ source/test-tools/matchers.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "core.hpp"
+#include <boost/core/demangle.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_templated.hpp>
+#include <optional>
+#include <sstream>
+#include <string>
+#include <type_traits>
+#include <variant>
+
+namespace cse498 {
+namespace matchers {
+template <typename T> struct OptNotNull : Catch::Matchers::MatcherGenericBase {
+  bool match(std::optional<T> const &other) const { return other.has_value(); }
+
+  std::string describe() const override { return "Has value (is not empty)"; }
+};
+
+template <typename State>
+struct VariantState : Catch::Matchers::MatcherGenericBase {
+  template <typename... Types>
+  bool match(std::variant<Types...> const &other) const {
+    return std::holds_alternative<State>(other);
+  }
+
+  // Gotta be a better way, like piping matcher results or something
+  template <typename... Types>
+  bool match(std::optional<std::variant<Types...>> other) const {
+    return std::holds_alternative<State>(other.value());
+  }
+
+  std::string describe() const override {
+    return std::format("Variant holds type '{}'",
+                       boost::core::demangle(typeid(State).name()));
+  }
+};
+
+}; // namespace matchers
+}; // namespace cse498
--- tests/core/Step.cpp
+++ tests/core/Step.cpp
@@ -1,10 +1,127 @@
 #include "core/Step.hpp"
+#include "WorldPosition.hpp"
+#include "Worlds/MazeWorld.hpp"
+#include "test-tools/matchers.hpp"
 
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_templated.hpp>
+#include <sstream>
+#include <string>
+#include <variant>
 
-TEST_CASE("InfoStep basic functionality", "[InfoStep]") {
+using namespace cse498::steps;
+
+TEST_CASE("StepContainer construction functionality", "[StepContainer]") {
+  using cse498::WorldPosition;
+
+  StepContainer steps{};
+
+  REQUIRE(steps.empty());
+  REQUIRE_FALSE(steps.root->step.has_value());
+
+  SECTION("Single movement container") {
+    steps.add_step(MovementStep{cse498::WorldPosition{0.5, 0.5}});
+
+    REQUIRE_FALSE(steps.empty());
+    REQUIRE(steps.last != nullptr);
+    REQUIRE(steps.last->step.has_value());
+    REQUIRE(std::holds_alternative<MovementStep>(steps.last->step.value()));
+  }
+
+  SECTION("Conditional movement") {
+    // Go to {1.5, 1.5} if the spot is free, otherwise go to {1.5, -1.5}
+
+    StepContainer t_branch{};
+    StepContainer f_branch{};
+
+    WorldPosition target{1.5, 1.5};
+    WorldPosition fallback{1.5, -1.5};
+
+    MovementStep target_move{target};
+    MovementStep fallback_move{fallback};
+    InfoStep can_move_info{InfoStep::Aspect::LOC_AVAIL, target};
+    ConditionalStep can_move_cond{[](bool b) { return b; }};
+
+    t_branch.add_step(std::move(target_move));
+    f_branch.add_step(std::move(fallback_move));
+
+    steps.add_step(std::move(can_move_info), std::move(can_move_cond),
+                   std::move(t_branch), std::move(f_branch));
+
+    REQUIRE_FALSE(steps.empty());
+
+    // Make sure t_branch and f_branch were moved from
+    REQUIRE(t_branch.empty());
+    REQUIRE(t_branch.root == nullptr);
+    REQUIRE(t_branch.last == nullptr);
+    REQUIRE(f_branch.empty());
+    REQUIRE(f_branch.root == nullptr);
+    REQUIRE(f_branch.last == nullptr);
+
+    // Check the steps are as expected
+    REQUIRE_FALSE(steps.root == nullptr);
+    REQUIRE_FALSE(steps.root->next == nullptr);
+    REQUIRE(steps.root->next->step.has_value());
+
+    // Future improvement -- this mess could be moved out to a custom Catch2
+    // matcher
+
+    StepContainer::Node const *checking_node = steps.root->next.get();
+
+    // Should see InfoStep
+    REQUIRE(checking_node->step.has_value());
+    REQUIRE_THAT(checking_node->step,
+                 cse498::matchers::OptNotNull<Step>() &&
+                     cse498::matchers::VariantState<InfoStep>());
+
+    InfoStep info_step = std::get<InfoStep>(checking_node->step.value());
+    REQUIRE(info_step.target == cse498::WorldPosition{1.5, 1.5});
+    REQUIRE(info_step.aspect == InfoStep::Aspect::LOC_AVAIL);
+
+    REQUIRE_FALSE(checking_node->next == nullptr);
+
+    checking_node = checking_node->next.get();
+
+    // Should see ConditionalStep
+    REQUIRE_THAT(checking_node->step,
+                 cse498::matchers::OptNotNull<Step>() &&
+                     cse498::matchers::VariantState<ConditionalStep>());
+
+    // Note - leave testing InfoHandler correctness to the InfoHandler tests
+
+    REQUIRE_FALSE(checking_node->left == nullptr);
+    REQUIRE_FALSE(checking_node->left->next == nullptr);
+    REQUIRE_FALSE(checking_node->right == nullptr);
+    REQUIRE_FALSE(checking_node->right->next == nullptr);
+
+    // Check left branch correctness
+    REQUIRE_THAT(checking_node->left->next->step,
+                 cse498::matchers::OptNotNull<Step>() &&
+                     cse498::matchers::VariantState<MovementStep>());
+
+    REQUIRE(
+        std::get<MovementStep>(checking_node->left->next->step.value()).loc ==
+        target);
+    REQUIRE(checking_node->left->next->next == nullptr);
+
+    // Check right branch correctness
+    REQUIRE_THAT(checking_node->right->next->step,
+                 cse498::matchers::OptNotNull<Step>() &&
+                     cse498::matchers::VariantState<MovementStep>());
+
+    REQUIRE(
+        std::get<MovementStep>(checking_node->right->next->step.value()).loc ==
+        fallback);
+    REQUIRE(checking_node->right->next->next == nullptr);
+
+    // No more steps after conditional
+    REQUIRE(checking_node->next == nullptr);
+  }
+};
+
+TEST_CASE("InfoStep and InfoHandler basic functionality", "[InfoStep]") {
   using namespace cse498::steps;
   InfoHandler test_handler(
       [](int i) -> std::expected<bool, StepErr> { return i == 0; });
   SECTION("default unexpected correctly returned for the invalid types") {}
-}
+};
