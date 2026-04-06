7f3cb04629b57f57faf6308d63625466be99814d
Luke Bridges <lukebridges123@gmail.com>
2026-04-05 21:11:30 -0400

    clang-format

--- source/tools/StateGrid/StateGrid.cpp
+++ source/tools/StateGrid/StateGrid.cpp
@@ -31,7 +31,8 @@ StateGrid::StateGrid(const std::vector<std::vector<char>> &premadeMap) {
 
 [[nodiscard]] size_t StateGrid::getHeight() const { return tiles.size(); }
 
-[[nodiscard]] bool StateGrid::inBoundsAndTraversable(size_t row, size_t col) const {
+[[nodiscard]] bool StateGrid::inBoundsAndTraversable(size_t row,
+                                                     size_t col) const {
   if (row >= getHeight() || col >= getWidth()) return false;
   return tiles[row][col].getCanTraverse();
 }
--- source/tools/WeightedSet.hpp
+++ source/tools/WeightedSet.hpp
@@ -91,7 +91,7 @@ class WeightedSet {
    * generate a random number, including in GetRandomElement which is a const
    * function.
    */
-  
+
   /**
    * @brief Maps elements to their nodes in the tree.
    * @remark Keys are elements stored in the WeightedSet; values are non-owning
