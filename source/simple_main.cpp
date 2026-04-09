24900fe9bfb59e8f6e558153577b70269b517bf8
johnnystouffer <stouff17@msu.edu>
2026-04-09 16:44:51 -0400

    fix: reverted back simple_main.cpp

--- source/simple_main.cpp
+++ source/simple_main.cpp
@@ -5,51 +5,25 @@
  **/
 
 // Include the modules that we will be using.
-#include "Agents/SwarmingAgent.hpp"
+#include "Agents/PacingAgent.hpp"
+#include "Interfaces/TrashInterface.hpp"
 #include "Worlds/MazeWorld.hpp"
-#include "core/AgentData.hpp"
 
 using namespace cse498;
 
-
-// // Still needs more work, gotta figure out how we want SwarmingAgents to act in
-// // the infectiious world, but this is a start
-// struct InfectionData {
-//   double infection_probability;
-//   cse498::HealthState infection_state;
-//   std::optional<cse498::WorldPosition> destination;
-//   cse498::WorldPosition position;
-//   cse498::Direction direction;
-// };
-
-
 int main() {
-    TrafficData test_data{
-        cse498::WorldPosition{5, 5},  // destination
-        cse498::WorldPosition{3, 1},  // position
-        false,
-        true
-    };
-    
   MazeWorld world;
- 
-//   world.AddAgent<PacingAgent>("Pacer 1").SetLocation(WorldPosition{3, 1});
-//   world.AddAgent<PacingAgent>("Pacer 2").SetLocation(WorldPosition{6, 1});
-//   world.AddAgent<PacingAgent>("Guard 1").SetHorizontal().SetLocation(
-//       WorldPosition{7, 7});
-//   world.AddAgent<PacingAgent>("Guard 2")
-//       .SetHorizontal()
-//       .ToggleDirection()
-//       .SetLocation(WorldPosition{8, 8});
-//   world.AddAgent<TrashInterface>("Interface")
-//       .SetSymbol('@')
-//       .SetLocation(WorldPosition{1, 1});
+  world.AddAgent<PacingAgent>("Pacer 1").SetLocation(WorldPosition{3, 1});
+  world.AddAgent<PacingAgent>("Pacer 2").SetLocation(WorldPosition{6, 1});
+  world.AddAgent<PacingAgent>("Guard 1").SetHorizontal().SetLocation(
+      WorldPosition{7, 7});
+  world.AddAgent<PacingAgent>("Guard 2")
+      .SetHorizontal()
+      .ToggleDirection()
+      .SetLocation(WorldPosition{8, 8});
+  world.AddAgent<TrashInterface>("Interface")
+      .SetSymbol('@')
+      .SetLocation(WorldPosition{1, 1});
 
-
-
-world.AddAgent<SwarmingAgent<TrafficData>>(test_data);
-world.Run();
-// clang-format -i -style=Google simple_main.cpp
-
-// winget install -e --id LLVM.LLVM
+  world.Run();
 }
