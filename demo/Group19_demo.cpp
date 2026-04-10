8e395ca1a9516f0e82333b9325e54e7ed713b7f3
Luke Bridges <lukebridges123@gmail.com>
2026-04-09 21:50:25 -0400

    Allow loading TrafficWorld grid from string vector or file

--- demo/Group19_demo.cpp
+++ demo/Group19_demo.cpp
@@ -10,14 +10,15 @@
 using namespace cse498;
 
 int main() {
-  // At this point, there isn't much you can do with TrafficWorld besides make it. 
-  // You can add agents in the same way as any world, but it spawns agents on its own, so you don't need to.
-  // Also note that TrafficWorld is only fully compatible with DrivingAgent right now. This is not something
-  // we want to have long-term, of course, but until the agent-world API is fully worked out, we figure we'll
-  // just use a single simple agent to show things off.
-  TrafficWorld world;
-  // AutoInterface is a little class we made to replace TrashInterface for the specific purpose
-  // of 
+  // At this point, there isn't much you can do with TrafficWorld besides make
+  // it. You can add agents in the same way as any world, but it spawns agents
+  // on its own, so you don't need to. Also note that TrafficWorld is only fully
+  // compatible with DrivingAgent right now. This is not something we want to
+  // have long-term, of course, but until the agent-world API is fully worked
+  // out, we figure we'll just use a single simple agent to show things off.
+  TrafficWorld world("DemoWorld.grid");
+  // AutoInterface is a little class we made to replace TrashInterface for the
+  // specific purpose of
   world.AddAgent<AutoInterface>("Viewer");
 
   world.Run();
