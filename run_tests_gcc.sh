a892ceb5f568d34485448f927bcf8274453f83f9
noyonikap-glitch <noyonikapuram@gmail.com>
2026-04-10 17:48:17 -0400

    removed run_tests_gcc

--- run_tests_gcc.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Run tests with GCC 15 and Xcode SDK (avoids _bounds.h error on macOS)
-SDKROOT=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk CXX=g++-15 make test
