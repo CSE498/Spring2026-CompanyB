c914056a865293fbc6b119ce27bc7d338c03f717
Cole Scheller <schelcc@pobox.com>
2026-04-12 19:15:27 -0400

    Individual files for parser work

---
+++ source/Interpreter/parse_agent_def.cpp
@@ -0,0 +1,8 @@
+#include "Interpreter/ast.hpp"
+#include "Parser.hpp"
+
+namespace cse498 {
+
+std::expected<std::unique_ptr<AST::Node>, ParseErr> Parser::parse_agent_def() {}
+
+}; // namespace cse498
---
+++ source/Interpreter/parse_assignment.cpp
@@ -0,0 +1,9 @@
+#include "Interpreter/ast.hpp"
+#include "Parser.hpp"
+
+namespace cse498 {
+
+std::expected<std::unique_ptr<AST::Node>, ParseErr> Parser::parse_assignment() {
+}
+
+}; // namespace cse498
