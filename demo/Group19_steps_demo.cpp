2c41282cf94d0b2be875a515f75d1f84b1b833c8
Luke Bridges <lukebridges123@gmail.com>
2026-04-13 16:01:50 -0400

    Remove test agent from demo (no longer needed now that we have spawners)

--- demo/Group19_steps_demo.cpp
+++ demo/Group19_steps_demo.cpp
@@ -3,12 +3,5 @@
 using namespace cse498;
 int main() {
   StepTrafficWorld world("DemoWorld.grid");
-  TrafficData test_data{WorldPosition{33, 3},
-                        WorldPosition{4, 1},
-                        Direction::East,
-                        true,
-                        '>',
-                        "\033[91m"};
-  world.AddAgent<StepDrivingAgent>(test_data);
   world.RunWithDisplay();
 }
