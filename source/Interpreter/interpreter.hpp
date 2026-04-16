54c62be3c7cf3c2c5499e6de25d946f004d6b36b
Cole Scheller <schelcc@pobox.com>
2026-04-16 19:10:06 -0400

    Correct InterpreterWrapper to use the correct dataclasses

--- source/Interpreter/interpreter.hpp
+++ source/Interpreter/interpreter.hpp
@@ -4,6 +4,7 @@
 #include "Interpreter/ast.hpp"
 #include "Interpreter/errors.hpp"
 #include "agentlang.hpp"
+#include "core/DiseaseData.hpp"
 #include "tools/RobinHoodMap.hpp"
 #include <any>
 #include <string>
@@ -13,10 +14,6 @@ using namespace cse498::agentlang::Types;
 
 template <typename T>
 concept Numeric = Concepts::IsOneOf<int, double>;
-
-// GET DATACLASSES IN THIS BRANCH SO ASAP, FOR NOW JUST DOING THIS TO DEMO
-struct InfectionData {};
-struct TrafficData {};
 
 template <typename Dataclass> class Interpreter;
 
@@ -44,7 +41,7 @@ struct InterpreterWrapper {
   }
 
   template <typename Dataclass> InterpreterWrapper(Interpreter<Dataclass> *i) {
-    if constexpr (std::is_same_v<Dataclass, InfectionData>) {
+    if constexpr (std::is_same_v<Dataclass, DiseaseData>) {
       m_Env = Env::INFECTION;
     } else {
       m_Env = Env::TRAFFIC;
@@ -62,7 +59,6 @@ template <typename Dataclass> class Interpreter {
       std::function<std::expected<Type, InterpErr>(Type, Type)>;
 
   OpVisits<Dataclass> m_OpVisits{};
-
   RobinHoodMap<int, OpFuncUnary> m_OpDispUnary{
       {emplex::Lexer::ID_CMP_GEQ, OpVisits<Dataclass>::m_OpVisit_Un_GEQ},
       {emplex::Lexer::ID_CMP_GT, OpVisits<Dataclass>::m_OpVisit_Un_GT},
