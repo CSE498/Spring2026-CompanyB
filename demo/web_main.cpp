2ed18eeb0100f2ba494289ca2a09cb5930b9c8b2
Varun Verma <vermava1@msu.edu>
2026-04-15 20:21:08 -0400

    Implemented File Upload Functionality & Tests
    Added: SetOnFileUpload; a new public method that turns a standard WebButton to a file picker.
    
    Implemented logic to generate a hidden <input type="file"> whcih is triggered when the 'Upload' button is pressed
    
    Integrated the browser's FileReader API to read any local files as text and convert into std::string
    
    Refactored the Constructor and Destructor to only attempt DOM clean-up if the objects were successfully initialized
    
    Update: web_main.cpp has now wired the "upload" button to the new file header, replacing the placeholder lambda with a callback that captures, and logs file content.
    
    Added: New test in test_WebButton.cpp that mocks a Javascript Filereader to verify the accuracy of the extracted string
    
    Tested using:
    make docker-test-emscripten: Passed all tests
    make docker-serve: When uploading a file, a 'success' message is printed in the browser's console(available through inspecting the page)

--- demo/web_main.cpp
+++ demo/web_main.cpp
@@ -98,7 +98,13 @@ std::shared_ptr<WebElement> SimulationLayout(std::function<void()> btn_lambda, s
           UIItem<WebButton>("", WebOptions{ .id = "start-btn", .classes={"button"} })->SetOnClick(btn_lambda),
           UIItem<WebButton>("", WebOptions{ .id = "pause-btn", .classes={"button"} })->SetOnClick(btn_lambda),
           UIItem<WebButton>("", WebOptions{ .id = "stop-btn", .classes={"button"} })->SetOnClick(btn_lambda),
-          UIItem<WebButton>("", WebOptions{ .id = "upload-btn", .classes={"button"} })->SetOnClick(btn_lambda),
+
+          // Using SetOnFileUpload with a custom lambda
+          UIItem<WebButton>("", WebOptions{ .id = "upload-btn", .classes={"button"} })->SetOnFileUpload([](const std::string& file_content) {
+              std::println("SUCCESS! File uploaded to C++ backend. Length: {} characters.", file_content.length());
+              // TODO: When the World object is available, do: world_ptr->LoadScript(file_content);
+          }),
+
           UIItem<WebButton>("", WebOptions{ .id = "save-btn", .classes={"button"} })->SetOnClick(btn_lambda),
           UIItem<WebButton>("", WebOptions{ .id = "exit-btn", .classes={"button"} })->SetOnClick(exit_lambda)
         }
@@ -111,10 +117,10 @@ std::shared_ptr<WebElement> SimulationLayout(std::function<void()> btn_lambda, s
       UIItem<WebLayout>(WebOptions{
         .id = "main-layout",
         .children = {
-          UIItem<WebLayout>(WebOptions{ 
+          UIItem<WebLayout>(WebOptions{
             .id = "left-column",
             .style = {
-              {"flex", "3"}, 
+              {"flex", "3"},
             },
 
             .children = {
