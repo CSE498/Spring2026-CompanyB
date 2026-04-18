37248b5dee98300083f010b1184e349e349722d0
Cole Scheller <schelcc@pobox.com>
2026-04-17 23:59:15 -0400

    Rework test to correct tree and use matchers

--- tests/interpreter/ExprParserTest.cpp
+++ tests/interpreter/ExprParserTest.cpp
@@ -8,12 +8,16 @@
 #include "Interpreter/Parser.hpp"
 #include "Interpreter/ast.hpp"
 #include "Interpreter/errors.hpp"
+#include "Interpreter/lexer.hpp"
+#include "test-tools/matchers.hpp"
 
 #include <sstream>
 #include <string>
 
 using namespace cse498;
 using namespace cse498::AST;
+using namespace cse498::matchers;
+using AgentLexer::IDs;
 
 // Parses the given statements under a "world traffic;" header.
 // Returns the parser (which holds m_Nodes and m_Syms) and the parse result.
@@ -31,18 +35,17 @@ TEST_CASE("Expr (Basic): 1+-1", "[expr][parser]") {
   REQUIRE(result.has_value());
   REQUIRE(p.m_Nodes.size() == 1);
 
-  auto *expr_binary = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
-  REQUIRE(expr_binary != nullptr);
+  auto *root = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
+  REQUIRE(root);
+  auto *left = dynamic_cast<ValLiteral *>(root->m_Left.get());
+  REQUIRE(left);
+  auto *right = dynamic_cast<ExprUnary *>(root->m_Right.get());
+  REQUIRE(right);
+  auto *right_left = dynamic_cast<ValLiteral *>(right->m_Left.get());
+  REQUIRE(right_left);
 
-  auto *val_literal = dynamic_cast<ValLiteral *>(expr_binary->m_Left.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 1);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary->m_Right.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == -1);
+  CHECK_THAT(left->m_Val, VariantHas<int>(1));
+  CHECK_THAT(right_left->m_Val, VariantHas<int>(1));
 }
 
 TEST_CASE("Expr (Ambiguity): 2-1+1", "[expr][parser]") {
@@ -53,28 +56,22 @@ TEST_CASE("Expr (Ambiguity): 2-1+1", "[expr][parser]") {
 
   // ExprBinary
 
-  auto *expr_binary = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
-  REQUIRE(expr_binary != nullptr);
+  auto *root = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
+  REQUIRE(root);
+  auto *left = dynamic_cast<ExprBinary *>(root->m_Left.get());
+  REQUIRE(left);
+  auto *right = dynamic_cast<ValLiteral *>(root->m_Right.get());
+  REQUIRE(right);
+  auto *left_left = dynamic_cast<ValLiteral *>(left->m_Left.get());
+  REQUIRE(left_left);
+  auto *left_right = dynamic_cast<ValLiteral *>(left->m_Right.get());
+  REQUIRE(left_right);
 
-  auto *val_literal = dynamic_cast<ValLiteral *>(expr_binary->m_Right.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 1);
-
-  // ExprBinary #2
-
-  auto *expr_binary_2 = dynamic_cast<ExprBinary *>(expr_binary->m_Left.get());
-  REQUIRE(expr_binary_2 != nullptr);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_2->m_Left.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 2);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_2->m_Right.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 1);
+  CHECK(root->m_Token.id == AgentLexer::IDs::ID_OP_ADD);
+  CHECK(left->m_Token.id == AgentLexer::IDs::ID_OP_MINUS);
+  CHECK_THAT(right->m_Val, VariantHas<int>(1));
+  CHECK_THAT(left_left->m_Val, VariantHas<int>(2));
+  CHECK_THAT(left_right->m_Val, VariantHas<int>(1));
 }
 
 TEST_CASE("Expr (Precedence): 2-3*4", "[expr][parser]") {
@@ -85,28 +82,22 @@ TEST_CASE("Expr (Precedence): 2-3*4", "[expr][parser]") {
 
   // ExprBinary
 
-  auto *expr_binary = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
-  REQUIRE(expr_binary != nullptr);
+  auto *root = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
+  REQUIRE(root);
+  auto *left = dynamic_cast<ValLiteral *>(root->m_Left.get());
+  REQUIRE(left);
+  auto *right = dynamic_cast<ExprBinary *>(root->m_Right.get());
+  REQUIRE(right);
+  auto *right_left = dynamic_cast<ValLiteral *>(right->m_Left.get());
+  REQUIRE(right_left);
+  auto *right_right = dynamic_cast<ValLiteral *>(right->m_Right.get());
+  REQUIRE(right_right);
 
-  auto *val_literal = dynamic_cast<ValLiteral *>(expr_binary->m_Left.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 2);
-
-  // ExprBinary #2
-
-  auto *expr_binary_2 = dynamic_cast<ExprBinary *>(expr_binary->m_Right.get());
-  REQUIRE(expr_binary_2 != nullptr);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_2->m_Left.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 3);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_2->m_Right.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 4);
+  CHECK(root->m_Token.id == AgentLexer::IDs::ID_OP_MINUS);
+  CHECK(right->m_Token.id == AgentLexer::IDs::ID_OP_MULT);
+  CHECK_THAT(left->m_Val, VariantHas<int>(2));
+  CHECK_THAT(right_left->m_Val, VariantHas<int>(3));
+  CHECK_THAT(right_right->m_Val, VariantHas<int>(4));
 }
 
 TEST_CASE("Expr (PMDAS): 1+2*(3+4)+5", "[expr][parser]") {
@@ -117,48 +108,36 @@ TEST_CASE("Expr (PMDAS): 1+2*(3+4)+5", "[expr][parser]") {
 
   // ExprBinary
 
-  auto *expr_binary = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
-  REQUIRE(expr_binary != nullptr);
+  auto *root = dynamic_cast<ExprBinary *>(p.m_Nodes[0].get());
+  REQUIRE(root);
+  auto *left = dynamic_cast<ExprBinary *>(root->m_Left.get());
+  REQUIRE(left);
+  auto *left_left = dynamic_cast<ValLiteral *>(left->m_Left.get());
+  REQUIRE(left_left);
+  auto *left_right = dynamic_cast<ExprBinary *>(left->m_Right.get());
+  REQUIRE(left_right);
+  auto *left_right_left = dynamic_cast<ValLiteral *>(left_right->m_Left.get());
+  REQUIRE(left_right_left);
+  auto *left_right_right =
+      dynamic_cast<ExprBinary *>(left_right->m_Right.get());
+  REQUIRE(left_right_right);
+  auto *left_right_right_left =
+      dynamic_cast<ValLiteral *>(left_right_right->m_Left.get());
+  REQUIRE(left_right_right_left);
+  auto *left_right_right_right =
+      dynamic_cast<ValLiteral *>(left_right_right->m_Right.get());
+  REQUIRE(left_right_right_right);
+  auto *right = dynamic_cast<ValLiteral *>(root->m_Right.get());
+  REQUIRE(right);
 
-  auto *val_literal = dynamic_cast<ValLiteral *>(expr_binary->m_Right.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 5);
+  CHECK(root->m_Token.id == IDs::ID_OP_ADD);
+  CHECK(left->m_Token.id == IDs::ID_OP_ADD);
+  CHECK(left_right->m_Token.id == IDs::ID_OP_MULT);
+  CHECK(left_right_right->m_Token.id == IDs::ID_OP_ADD);
 
-  // ExprBinary #2
-
-  auto *expr_binary_2 = dynamic_cast<ExprBinary *>(expr_binary->m_Left.get());
-  REQUIRE(expr_binary_2 != nullptr);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_2->m_Left.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 1);
-
-  // ExprBinary #3
-
-  auto *expr_binary_3 =
-      dynamic_cast<ExprBinary *>(expr_binary_2->m_Right.get());
-  REQUIRE(expr_binary_3 != nullptr);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_3->m_Left.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 2);
-
-  // ExprBinary #4
-
-  auto *expr_binary_4 =
-      dynamic_cast<ExprBinary *>(expr_binary_3->m_Right.get());
-  REQUIRE(expr_binary_4 != nullptr);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_4->m_Left.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 3);
-
-  val_literal = dynamic_cast<ValLiteral *>(expr_binary_4->m_Right.get());
-  REQUIRE(val_literal != nullptr);
-  CHECK(std::holds_alternative<int>(val_literal->m_Val));
-  CHECK(std::get<int>(val_literal->m_Val) == 4);
+  CHECK_THAT(left_left->m_Val, VariantHas<int>(1));
+  CHECK_THAT(left_right_left->m_Val, VariantHas<int>(2));
+  CHECK_THAT(left_right_right_left->m_Val, VariantHas<int>(3));
+  CHECK_THAT(left_right_right_right->m_Val, VariantHas<int>(4));
+  CHECK_THAT(right->m_Val, VariantHas<int>(5));
 }
