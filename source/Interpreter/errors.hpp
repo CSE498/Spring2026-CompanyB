a13221e72935a75ea0aa80b543623356cd13a715
Cole Scheller <schelcc@pobox.com>
2026-04-18 14:16:50 -0400

    Improve error string representation

--- source/Interpreter/errors.hpp
+++ source/Interpreter/errors.hpp
@@ -1,8 +1,14 @@
 #pragma once
+#include <array>
+#include <concepts>
 #include <expected>
+#include <format>
+#include <ostream>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <variant>
+
 namespace cse498 {
 
 struct BaseErr {
@@ -17,116 +23,143 @@ struct BaseErr {
   }
 
   bool operator==(const BaseErr &) const = default;
-
-  // template <typename Self>
-  // bool operator==(this Self const &self, Self const &other) {
-  //   return ((self.why_ == other.why_) && (self.m_Kind == other.m_Kind));
-  // }
 };
+static std::string Name(std::string const &base, std::string_view const &state,
+                        std::string const &msg = "") {
+  return std::format("{}::{}({})", base, state, msg);
+}
 
 struct ASTErr : BaseErr {
   bool operator==(const ASTErr &) const = default;
   enum Kind {
-    UNTYPED_NODE,
+    UNTYPED_NODE = 0,
   };
+
+  static constexpr std::array<std::string_view, 1> m_KindNames = {
+      "UNTYPED_NODE"};
 
   Kind m_Kind;
 
-  ASTErr(Kind kind) : BaseErr("ASTErr"), m_Kind(kind) {};
+  ASTErr(Kind kind)
+      : BaseErr(Name("ASTErr", m_KindNames[kind])), m_Kind(kind) {};
   ASTErr(Kind kind, std::string const &why)
-      : BaseErr("ASTErr: " + why), m_Kind(kind) {};
+      : BaseErr(Name("ASTErr", m_KindNames[kind], why)), m_Kind(kind) {};
 };
 
 struct LexerErr : BaseErr {
   bool operator==(const LexerErr &) const = default;
   enum Kind {
-    UNEXP_TOKEN,
+    UNEXP_TOKEN = 0,
   };
+
+  static constexpr std::array<std::string_view, 1> m_KindNames = {
+      "UNEXP_TOKEN"};
 
   Kind m_Kind;
 
-  LexerErr(Kind kind) : BaseErr("LexerErr"), m_Kind(kind) {}
+  LexerErr(Kind kind)
+      : BaseErr(Name("LexerErr", m_KindNames[kind])), m_Kind(kind) {}
   LexerErr(Kind kind, std::string const &msg)
-      : BaseErr("LexerErr: " + msg), m_Kind(kind) {}
+      : BaseErr(Name("LexerErr", m_KindNames[kind], msg)), m_Kind(kind) {}
 };
 
 struct ParseErr : BaseErr {
   bool operator==(const ParseErr &) const = default;
   enum Kind {
-    MISSING_TOKEN,
-    EXPECTED_STMT,
-    OUT_OF_TURN,
-    OUT_OF_INIT,
-    OUT_OF_LOOP,
-    INVALID_OP_TOKEN,
-    INVALID_LITERAL,
-    INVALID_TERM,
-    INVALID_WORLD,
-    AT_EOF,
+    MISSING_TOKEN = 0,
+    EXPECTED_STMT = 1,
+    OUT_OF_TURN = 2,
+    OUT_OF_INIT = 3,
+    OUT_OF_LOOP = 4,
+    INVALID_OP_TOKEN = 5,
+    INVALID_LITERAL = 6,
+    INVALID_TERM = 7,
+    INVALID_WORLD = 8,
+    AT_EOF = 9,
   };
+  static constexpr std::array<std::string_view, 10> m_KindNames = {
+      "MISSING_TOKEN",   "EXPECTED_STMT", "OUT_OF_TURN",
+      "OUT_OF_INIT",     "OUT_OF_LOOP",   "INVALID_OP_TOKEN",
+      "INVALID_LITERAL", "INVALID_TERM",  "INVALID_WORLD",
+      "AT_EOF"};
 
   Kind m_Kind;
-  ParseErr(Kind kind) : BaseErr("ParseErr"), m_Kind(kind) {}
+  ParseErr(Kind kind)
+      : BaseErr(Name("ParseErr", m_KindNames[kind])), m_Kind(kind) {}
   ParseErr(Kind kind, std::string const &msg)
-      : BaseErr("ParseErr: " + msg), m_Kind(kind) {}
+      : BaseErr(Name("ParseErr", m_KindNames[kind], msg)), m_Kind(kind) {}
 };
 
 struct SymbolErr : public BaseErr {
   bool operator==(const SymbolErr &) const = default;
   enum Kind {
-    UNDEFINED_SYMBOL,
-    REDEFINITION,
-    INVALID_TYPE,
+    UNDEFINED_SYMBOL = 0,
+    REDEFINITION = 1,
+    INVALID_TYPE = 2,
   };
+  static constexpr std::array<std::string_view, 3> m_KindNames = {
+      "UNDEFINED_SYMBOL", "REDEFINITION", "INVALID_TYPE"};
 
   Kind m_Kind;
-  SymbolErr(Kind kind) : BaseErr("SymbolErr"), m_Kind(kind) {}
+  SymbolErr(Kind kind)
+      : BaseErr(Name("SymbolErr", m_KindNames[kind])), m_Kind(kind) {}
   SymbolErr(Kind kind, std::string const &msg)
-      : BaseErr("SymbolErr: " + msg), m_Kind(kind) {}
+      : BaseErr(Name("SymolErr", m_KindNames[kind], msg)), m_Kind(kind) {}
 };
 
 struct RuntimeErr : BaseErr {
   bool operator==(const RuntimeErr &) const = default;
   enum Kind {
-    TYPE_MISMATCH,
-    EMPTY_INTERP_WRAPPER,
-    UNSUPPORTED_OP,
-    NOT_BOOL_CONV,
-    ENCOUNTERED_AGENT_DEF,
-    INVALID_MOVE_ARG,
-    TOO_MANY_MOVES
+    TYPE_MISMATCH = 0,
+    EMPTY_INTERP_WRAPPER = 1,
+    UNSUPPORTED_OP = 2,
+    NOT_BOOL_CONV = 3,
+    ENCOUNTERED_AGENT_DEF = 4,
+    INVALID_MOVE_ARG = 5,
+    TOO_MANY_MOVES = 6
   };
+  static constexpr std::array<std::string_view, 7> m_KindNames = {
+      "TYPE_MISMATCH", "EMPTY_INTERP_WRAPPER",  "UNSUPPORTED_OP",
+      "NOT_BOOL_CONV", "ENCOUNTERED_AGENT_DEF", "INVALID_MOVE_ARG",
+      "TOO_MANY_MOVES"};
 
   Kind m_Kind;
-  RuntimeErr(Kind kind) : BaseErr("RuntimeErr"), m_Kind(kind) {}
+  RuntimeErr(Kind kind)
+      : BaseErr(Name("RuntimeErr", m_KindNames[kind])), m_Kind(kind) {}
   RuntimeErr(Kind kind, std::string const &msg)
-      : BaseErr("RuntimeErr: " + msg), m_Kind(kind) {}
+      : BaseErr(Name("RuntimeErr", m_KindNames[kind], msg)), m_Kind(kind) {}
 };
 
 struct LoopControlErr : BaseErr {
   bool operator==(const LoopControlErr &) const = default;
   enum Kind {
-    BREAK,
-    CONTINUE,
+    BREAK = 0,
+    CONTINUE = 1,
   };
+  static constexpr std::array<std::string_view, 2> m_KindNames = {"BREAK",
+                                                                  "CONTINUE"};
 
   Kind m_Kind;
-  LoopControlErr(Kind kind) : BaseErr("LoopControlErr"), m_Kind(kind) {}
+  LoopControlErr(Kind kind)
+      : BaseErr(Name("LoopControlErr", m_KindNames[kind])), m_Kind(kind) {}
   LoopControlErr(Kind kind, std::string const &msg)
-      : BaseErr("LoopControlErr: " + msg), m_Kind(kind) {}
+      : BaseErr(Name("LoopControlErr", m_KindNames[kind], msg)), m_Kind(kind) {}
 };
 
 // For internal errors as we implement the rest of the interpreter
 struct TempErr : BaseErr {
   bool operator==(const TempErr &) const = default;
   enum Kind {
-    NOT_IMPLEMENTED,
+    NOT_IMPLEMENTED = 0,
   };
+  static constexpr std::array<std::string_view, 1> m_KindNames = {
+      "NOT_IMPLEMENTED"};
 
   Kind m_Kind;
-  TempErr(Kind kind) : BaseErr("TempErr"), m_Kind(kind) {}
+  TempErr(Kind kind)
+      : BaseErr(Name("TempErr", m_KindNames[kind])), m_Kind(kind) {}
   TempErr(Kind kind, std::string const &msg)
-      : BaseErr("TempErr: " + msg), m_Kind(kind) {}
+      : BaseErr(Name("TempErr", m_KindNames[kind], msg)), m_Kind(kind) {}
 };
 
 using InterpErr_T = std::variant<LexerErr, ParseErr, SymbolErr, ASTErr,
