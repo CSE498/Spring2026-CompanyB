64313bafdc90877a4597d9fc6cc18fee777b9035
Abdulrahman Alanazi <alanazi7@msu.edu>
2026-04-19 16:56:17 -0400

    fix merge conflict

--- source/core/Step.hpp
+++ source/core/Step.hpp
@@ -18,6 +18,7 @@ namespace cse498 {
 namespace steps {
 
 using InfoType = std::variant<int, double, bool>;
+using _InfoTuple = std::variant<int, double, bool>;
 
 template <typename T>
 concept IsInfoType = Concepts::IsOneOf<T, int, double, bool>;
@@ -33,27 +34,25 @@ struct StepErr {
 
   Kind kind;
   std::string msg;
-
-  StepErr() = delete;
-  StepErr(Kind in_kind) : kind(in_kind), msg("") {};
-  StepErr(Kind in_kind, std::string const &in_msg)
-      : kind(in_kind), msg(in_msg) {};
 };
 
 template <IsInfoType I>
 using InfoFunc = std::function<std::expected<bool, StepErr>(I)>;
 
-template <IsInfoType... Is> using InfoFuncTuple = std::tuple<InfoFunc<Is>...>;
+template <IsInfoType... Is>
+using InfoFuncTuple = std::tuple<InfoFunc<Is>...>;
 
 // Parameterize the info handler so that we can extremely easily add more types
 // later
-template <typename... Ts> struct _InfoHandler {
+template <typename... Ts>
+struct _InfoHandler {
   // Generate the default handler function
-  template <IsInfoType T> static InfoFunc<T> defaulted_handler() {
+  template <IsInfoType T>
+  static InfoFunc<T> defaulted_handler() {
     return [](T) {
       return std::unexpected(
-          StepErr(StepErr::Kind::WRONG_TYPE,
-                  "InfoStep passed invalidly typed InfoType into InfoHandler"));
+          StepErr{StepErr::Kind::WRONG_TYPE,
+                  "InfoStep passed invalidly typed InfoType into InfoHandler"});
     };
   }
 
@@ -76,13 +75,10 @@ template <typename... Ts> struct _InfoHandler {
   _InfoHandler(F f)
       : funcs({pick_handler<typename std::tuple_element<
                                 0, typename FuncInfo::FuncInfo<F>::args>::type,
-<<<<<<< HEAD
-                            Ts>(f)...}) {}
-=======
                             Ts>(f)...}){};
->>>>>>> origin/Group12-AgentBaseDataLogging
 
-  template <typename S> std::expected<bool, StepErr> operator()(S s) {
+  template <typename S>
+  std::expected<bool, StepErr> operator()(S s) {
     return std::invoke(std::get<InfoFunc<S>>(funcs), s);
   }
 };
@@ -90,30 +86,34 @@ template <typename... Ts> struct _InfoHandler {
 // Now set our desired InfoHandler type and let the compiler handle the rest
 using InfoHandler = _InfoHandler<int, double, bool>;
 
+template <IsInfoType I>
+std::function<std::expected<bool, StepErr>(I)> default_call() {
+  return [](I) {
+    std::unexpected(StepErr{StepErr::Kind::WRONG_TYPE,
+                            "InfoStep invalid type InfoType into InfoHandler"});
+  };
+}
+
 // Forward decl.
 struct StepContainer;
 
 struct MovementStep {
   WorldPosition loc;
 
-  bool operator==(MovementStep const& other) const { return other.loc == loc; }
+  bool operator==(MovementStep const& other) { return other.loc == loc; }
 };
 
 struct InfoStep {
   enum class Aspect {
-    OCCUPANCY_RAW,  // How many in area?
-    OCCUPANCY_FRAC, // How much of area is occupied?
-    LOC_AVAIL,      // Is specific spot available?
+    OCCUPANCY_RAW,   // How many in area?
+    OCCUPANCY_FRAC,  // How much of area is occupied?
+    LOC_AVAIL,       // Is specific spot available?
   };
 
   Aspect aspect;
   WorldPosition target;
 
-  InfoStep() = delete;
-  InfoStep(Aspect in_aspect, WorldPosition in_pos)
-      : aspect(in_aspect), target(in_pos) {};
-
-  bool operator==(InfoStep const &other) const {
+  bool operator==(InfoStep const& other) const {
     return (other.aspect == aspect) && (other.target == target);
   }
 };
@@ -121,17 +121,15 @@ struct InfoStep {
 struct ConditionalStep {
   InfoHandler condition;
 
-  bool operator==([[maybe_unused]] ConditionalStep const &other) const {
-    // TODO: Decide how to compare functors
+  bool operator==(ConditionalStep const& other) const {
+    // Figure out how to compare functors...
     return true;
   }
 };
 
 struct ReconStep {
   // TODO (probably gonna scrap)
-  bool operator==([[maybe_unused]] ReconStep const &other) const {
-    return false;
-  }
+  bool operator==(ReconStep const& other) const { return false; }
 };
 
 template <typename T>
@@ -152,12 +150,6 @@ struct StepContainer {
     std::unique_ptr<Node> next;
     std::unique_ptr<Node> left;
     std::unique_ptr<Node> right;
-
-    Node() : step({}), next(nullptr), left(nullptr), right(nullptr) {}
-
-    template <StepKind S>
-    Node(S &&s)
-        : step(std::move(s)), next(nullptr), left(nullptr), right(nullptr) {}
   };
 
   // Root is an "empty" node so that "last" can always be bound
@@ -183,11 +175,13 @@ struct StepContainer {
     return (next_stack.empty() && (cur_node == nullptr));
   }
 
-  template <IsInfoType I> void inform(I const &info) {
+  template <IsInfoType I>
+  void inform(I const& info) {
     world_info = InfoType{std::in_place_type<I>, info};
   }
 
-  template <StepKind S> void add_step(S &&s) {
+  template <StepKind S>
+  void add_step(S&& s) {
     assert(last != nullptr);
     last->next = std::make_unique<Node>(std::move(s));
     last = last->next.get();
@@ -236,7 +230,7 @@ struct StepContainer {
       last->right = std::move(f_body.root);
       f_body.last = nullptr;
     } else {
-      // F is just a step, so put it on last's right
+      // T is just a step, so put it on last's left
       last->right = std::make_unique<Node>();
       last->right->next = std::make_unique<Node>(std::move(f_body));
     }
@@ -260,7 +254,7 @@ struct StepContainer {
         return get_next();
       } else
         return std::unexpected(
-            StepErr(StepErr::Kind::STEPS_EXHAUSTED, "No remaining steps"));
+            StepErr{StepErr::Kind::STEPS_EXHAUSTED, "No remaining steps"});
     }
 
     // Skip empty step(s)
@@ -279,8 +273,8 @@ struct StepContainer {
       // Our world_info object must have information
       if (!world_info.has_value())
         return std::unexpected(
-            StepErr(StepErr::Kind::NOT_INFORMED,
-                    "StepContainer was not informed for ConditionalStep"));
+            StepErr{StepErr::Kind::NOT_INFORMED,
+                    "StepContainer was not informed for ConditionalStep"});
 
       ConditionalStep cur_step =
           std::get<ConditionalStep>(cur_node->step.value());
@@ -294,16 +288,15 @@ struct StepContainer {
       world_info = {};
 
       // Forward the error if encountered
-      if (!cond_result.has_value())
-        return std::unexpected(cond_result.error());
+      if (!cond_result.has_value()) return std::unexpected(cond_result.error());
 
       if (cond_result.value()) {
         // Likely don't need this check, it *should* be impossible to have a
         // conditional step w/o a true branch
         if (cur_node->left == nullptr)
-          return std::unexpected(StepErr(StepErr::Kind::MISSING_BRANCH,
+          return std::unexpected(StepErr{StepErr::Kind::MISSING_BRANCH,
                                          "ConditionalStep which evaluated true "
-                                         "does not have a true branch"));
+                                         "does not have a true branch"});
 
         // Push the return point onto the stack, then return the first step in
         // the true branch
@@ -335,5 +328,5 @@ struct StepContainer {
   }
 };
 
-}; // namespace steps
-}; // namespace cse498
+};  // namespace steps
+};  // namespace cse498
