615700c0d9d80ba17f47cf06b2cc216b83ee81d8
Luke Bridges <lukebridges123@gmail.com>
2026-04-28 15:33:44 -0400

    Fixes to help build our demo + access grid

--- demo/Group19_steps_demo_main.cpp
+++ demo/Group19_steps_demo_main.cpp
@@ -2,7 +2,7 @@
 #include "../source/Worlds/StepTrafficWorld.hpp"
 using namespace cse498;
 int main() {
-  StepTrafficWorld<SwarmingAgent<TrafficData>> world("DemoWorld.grid");
+  StepTrafficWorld<SwarmingAgent<TrafficData>> world("assets/DemoWorld.grid");
   //StepTrafficWorld<SwarmingAgent<TrafficData>> world("TinyWorld.grid");
   world.RunWithDisplay();
 }
