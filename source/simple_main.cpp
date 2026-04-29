359a89dcf6287af94fcafb97e94cf0d8e018d2f3
kiewskim <kiewskim@msu.edu>
2026-04-28 22:33:14 -0400

    Forgot to run clang-format

--- source/simple_main.cpp
+++ source/simple_main.cpp
@@ -1,8 +1,8 @@
 /**
-* This file is part of the Fall 2026, CSE 498, section 2, course project.
-* @brief A simplistic main file to demonstrate a system.
-* @note Status: PROPOSAL
-**/
+ * This file is part of the Fall 2026, CSE 498, section 2, course project.
+ * @brief A simplistic main file to demonstrate a system.
+ * @note Status: PROPOSAL
+ **/
 
 // Include the modules that we will be using.
 #include "Agents/PacingAgent.hpp"
